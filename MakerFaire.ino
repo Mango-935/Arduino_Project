1d6f6ed3e8ba73860397b48c4ebe44da773bfbc7
CJ Danyluk <Nickdan5743@gmail.com>
2021-11-30 16:05:03 -0500

    Updating Input

--- MakerFaire.ino
+++ MakerFaire.ino
@@ -1,38 +1,55 @@
-const int b1 = 2;
-const int b2 = 3;
-const int b3 = 4;
-const int b4 = 5;
-int sb1 = 0;
-int sb2 = 0;
-int sb3 = 0;
-int sb4 = 0;
+//#include <LiquidCrystal.h>
+//LiquidCrystal lcd();
+const int b1 = 2; //button 1
+const int b2 = 3; //button 2
+const int b3 = 4; //button 3
+const int b4 = 5; //button 4
+int sb1 = 0; //button 1 value
+int sb2 = 0; //button 2 value
+int sb3 = 0; //button 3 value
+int sb4 = 0; //button 4 value
+int prev_dir = 0; //previous direction
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
+  
+  //initialize button pins
+  pinMode(b1, INPUT);
+  pinMode(b2, INPUT);
+  pinMode(b3, INPUT);
+  pinMode(b4, INPUT);
 }
 
 void loop() {
+  //reads button values
   sb1 = digitalRead(b1);
   sb2 = digitalRead(b2);
   sb3 = digitalRead(b3);
   sb4 = digitalRead(b4);
-  if (sb1 == HIGH) {
-    Serial.print("1");
-  }
-  if (sb2 == HIGH) {
-    Serial.print("2");
-  }
-  if (sb3 == HIGH) {
-    Serial.print("3");
-  }
-  if (sb4 == HIGH) {
-    Serial.print("4");
-  }
-  Serial.println("");
+  /*
+  //reads score value
+  if (Serial.available()) //if data is available to read
+    
+  */
+  //determine current direction
+  int dir = 0;
+  if (sb1 == HIGH && prev_dir != 1 && prev_dir != 5)
+    dir = 1; //right
+  if (sb2 == HIGH && prev_dir != 2 && prev_dir != 5)
+    dir = 2; //down
+  if (sb3 == HIGH && prev_dir != 3 && prev_dir != 5)
+    dir = 3; //up
+  if (sb4 == HIGH && prev_dir != 4 && prev_dir != 5)
+    dir = 4; //left
+
+  if (dir == 0)
+    dir = prev_dir; //if no direction is inputted, use previous direction
+  else
+    prev_dir = dir; //if new direction is inputted, use new direction
+
+  Serial.println(dir);
+
+
   delay(1);
 }
--- MakerFaire/MakerFaire.pde
+++ MakerFaire/MakerFaire.pde
@@ -1,14 +1,7 @@
 import processing.serial.*;
 Serial myPort;
-
-final int b1 = 2;
-final int b2 = 3;
-final int b3 = 4;
-final int b4 = 5;
-int sb1 = 0;
-int sb2 = 0;
-int sb3 = 0;
-int sb4 = 0;
+String val;
+int score = 0;
 
 void setup() {
   fullScreen();
@@ -20,27 +13,9 @@ void setup() {
 }
 
 void draw() {
-  /*
-  //sb1 = digitalRead(b1);
-  //sb2 = digitalRead(b2);
-  //sb3 = digitalRead(b3);
-  //sb4 = digitalRead(b4);
-  if (sb1 == HIGH) {
-    Serial.print("1");
+  if (myPort.available() > 0) {
+    val = myPort.readStringUntil('\n'); //read input from arduino (the println statement)
+    //myPort.write(score); //send score to arduino
   }
-  if (sb2 == HIGH) {
-    Serial.print("2");
-  }
-  if (sb3 == HIGH) {
-    Serial.print("3");
-  }
-  if (sb4 == HIGH) {
-    Serial.print("4");
-  }
-  Serial.println("");
-  delay(1);
-  */
-  sb1 = myPort.read();
-  println(sb1);
-  delay(1);
+  println(val);
 }
